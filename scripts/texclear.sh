7ded3922e27e43ba0cc1a15050ee7e181ce3fe36
pfnaibert <paulo.naibert@gmail.com>
2020-08-16 18:23:05 -0300

    updated description in texclear.sh

--- scripts/texclear.sh
+++ scripts/texclear.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# list latex build files
+# list and remove latex build files
 
 echo $'Files to be deleted:'
 ls -lX *.aux *.log *.out *.synctex.gz *.bbl *.blg
 echo $'\nDeleting Now\n'
 find ./ -maxdepth 1 -type f \( -name '*.aux' -o -name '*.log' -o -name '*.out' -o -name '*.synctex.gz' -o -name '*.bbl' -o -name '*.blg' \) -delete
 echo $'Remaining Files:'
-ls -lX 
+ls -lX
